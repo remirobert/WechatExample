9f6bef08d7e9db38164fa7e506dc68a7bf81560a
rémi  <remirobert33530@gmail.com>
2016-10-26 11:41:05 +0200

    update for offline sharing

--- Wechat-SDK-Sample/ViewController.m
+++ Wechat-SDK-Sample/ViewController.m
@@ -14,6 +14,51 @@
 
 @implementation ViewController
 
+- (void)selectScene:(NSString *)title completion:(void (^)(int scene))completion {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    [alertController addAction:[UIAlertAction actionWithTitle:@"moment" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completion(WXSceneTimeline);
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"contacts" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completion(WXSceneSession);
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        completion(-1);
+    }]];
+    [self presentViewController:alertController animated:true completion:nil];
+}
+
+- (IBAction)shareLink:(id)sender {
+    [self selectScene:@"share link" completion:^(int scene) {
+        if (scene == -1) {
+            return ;
+        }
+        [[AuthWechatManager shareInstance] shareLinkWithTitle:@"title link"
+                                                  description:@"description link"
+                                                   thumbImage:[UIImage imageNamed:@"weixin"]
+                                                          url:@"http://www.google.com" to:scene];
+    }];
+}
+
+- (IBAction)shareText:(id)sender {
+    [self selectScene:@"share text" completion:^(int scene) {
+        if (scene == -1) {
+            return ;
+        }
+        [[AuthWechatManager shareInstance] shareTextWithText:@"text" to:scene];
+    }];
+}
+
+- (IBAction)sharePicture:(id)sender {
+    [self selectScene:@"share link" completion:^(int scene) {
+        if (scene == -1) {
+            return ;
+        }
+        [[AuthWechatManager shareInstance] shareImageWithImage:[UIImage imageNamed:@"weixin"] to:scene];
+    }];
+}
+
 - (IBAction)auth:(id)sender {
     [[AuthWechatManager shareInstance] auth: self];
 }
